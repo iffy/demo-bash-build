a9065b2836cda0e503aca7982782e5ccac66f6c8
Matt Haggard <haggardii@gmail.com>
2014-03-10 10:31:13 -0600

    css

--- build.sh
+++ build.sh
@@ -19,7 +19,9 @@ function minmtime() {
 function outofdate() {
     INPUT_FILES=$1
     OUTPUT_FILES=$2
-    if [ $(minmtime $INPUT_FILES) -gt $(minmtime $OUTPUT_FILES) ]; then
+    IN_MIN=$(minmtime $INPUT_FILES)
+    OUT_MIN=$(minmtime $OUTPUT_FILES)
+    if [ $IN_MIN -gt $OUT_MIN ]; then
         return 1
     else
         return 0
@@ -41,10 +43,44 @@ function sansext() {
 #------------------------------------------------------------------------------
 # app specific
 #------------------------------------------------------------------------------
-function installBower() {
+
+function do_npm() {
+    if $(outofdate node_modules package.json); then
+        npm install
+    fi
+}
+
+function do_bower() {
     if $(outofdate bower_components bower.json); then
         bower install
     fi
 }
 
-$1
+# scss -> intermediate, unminified css
+function do_scss() {
+    mkdir -p css
+    if $(outofdate css/base.css scss/base.scss); then
+        sass scss/base.scss css/base.css
+    fi
+}
+
+# scss -> minified css
+function do_css() {
+    mkdir -p production/css
+    do_scss
+    if $(outofdate production/css/base.css css/base.css); then
+        cssmin css/base.css > production/css/base.css
+    fi
+}
+
+function do_all() {
+    do_npm
+    do_bower
+    do_scss
+    do_css
+}
+
+PATH=$(pwd)/node_modules/.bin:${PATH}
+
+CMD=${1:-all}
+do_$CMD
